e1a28bf370410e667440a3973fb87c00aa2d82dc
Aastha Chauhan <chauhanaastha1810@gmail.com>
2026-06-27 17:37:55 +0530

    6-27-26

--- server/src/data/users.ts
+++ server/src/data/users.ts
@@ -11,5 +11,6 @@ export const users: User[] = [
   { id: "EMP002", username: "emp002", password: "CV@Emp234", name: "Employee 002" },
   { id: "EMP003", username: "emp003", password: "CV@Emp345", name: "Employee 003" },
   { id: "EMP004", username: "emp004", password: "CV@Emp456", name: "Employee 004" },
+  { id: "EMP006", username: "emp006", password: "CV@Emp457", name: "Employee 005" },
    { id: "EMP005", username: "spartan", password: "Spartan@123", name: "Spartan@001" },
 ];
